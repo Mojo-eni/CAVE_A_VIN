07b0e55f74d3975eb21b2f773571cea58da8af2e
Antoine Lucas <antoine.lucas2022@campus-eni.fr>
2025-04-16 09:40:02 +0200

    Security!

--- src/main/java/fr/eni/cave/security/SecurityConfig.java
+++ src/main/java/fr/eni/cave/security/SecurityConfig.java
@@ -1,14 +1,44 @@
 package fr.eni.cave.security;
 
+import org.apache.catalina.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
-     public void userDetailsManager(DataSource dataSource){
-         UserDetailsManager
-         return UserDetailsManager
+
+    @Bean
+     public UserDetailsManager userDetailsManager(DataSource dataSource){
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery("SELECT pseudo, password, 1 FROM CAV_USER WHERE pseudo=?");
+        userDetailsManager.setAuthoritiesByUsernameQuery("SELECT pseudo, authority FROM CAV_USER WHERE pseudo=?");
+         return  userDetailsManager;
+     }
+
+     @Bean
+     SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+         httpSecurity.authorizeHttpRequests(auth -> {
+             auth
+                     .requestMatchers("/caveavin/bouteilles").permitAll()
+                     .requestMatchers(HttpMethod.GET, "/caveavin/bouteilles/**").hasAnyRole("CLIENT", "ADMIN")
+                     .requestMatchers(HttpMethod.POST, "/caveavin/bouteilles").hasAnyRole("CLIENT", "ADMIN")
+                     .requestMatchers(HttpMethod.PUT, "/caveavin/bouteilles/**").hasAnyRole("CLIENT", "ADMIN")
+                     .anyRequest().denyAll();
+         });
+
+         httpSecurity.httpBasic(Customizer.withDefaults());
+         return httpSecurity.build();
      }
 }
 
